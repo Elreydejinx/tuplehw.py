7522d720608f7c27fcaf8c5a3b5324313ab016d1
Jeremiah-Williams <jae4lani17@gmail.com>
2024-06-21 10:23:45 -0400

    tuplehw.py

--- tuplehw.py
+++ tuplehw.py
@@ -11,3 +11,13 @@ def itinerary_printout(itinerary):
 
 
 itinerary_printout(itinerary)
+
+# Library System Enhancement
+
+library = [("1984", "George Orwell"), ("Brave New World", "Aldous Huxley")]
+new_books = library.append(input("what book would you like to add to your library:")), library.append(("The Miseducation of the Negro", "Carter G Woodson"))
+
+if ("1984","Brave New World","A whole new world", "The Miseducation of the Negro") in library:
+    print("These titles are already in library") 
+
+print(library)
